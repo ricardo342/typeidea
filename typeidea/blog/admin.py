42abc04c99323b4e39b5cf0c26dfe3b70d7d974f
ricardo342 <63143873+ricardo342@users.noreply.github.com>
2023-01-03 11:11:58 +0800

    Delete typeidea/blog directory

--- typeidea/blog/admin.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf-8 -*-
-#!/usr/bin/python3
-'''
-@File: admin
-@time:2023/1/3
-@Author:majiaqin 170479
-@Desc:
-'''
--- typeidea/blog/models.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf-8 -*-
-#!/usr/bin/python3
-'''
-@File: models
-@time:2023/1/3
-@Author:majiaqin 170479
-@Desc:
-'''
