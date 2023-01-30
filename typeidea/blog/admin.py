98c4d77d291c009768cc051f2d396da16dedcb1e
majiaqin <majiaqin@techb.top>
2023-01-30 09:58:37 +0800

    重写抽象出BaseOwnerAdmin

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -4,6 +4,7 @@ from django.utils.html import format_html
 
 from .models import Post, Category, Tag
 from .adminforms import PostAdminForm
+from typeidea.base_admin import BaseOwnerAdmin
 from typeidea.custom_site import custom_site
 
 # Register your models here.
@@ -64,7 +65,7 @@ class CategoryOwnerFilter(admin.SimpleListFilter):
         return queryset
 
 @admin.register(Post, site=custom_site)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(BaseOwnerAdmin):
     form = PostAdminForm
     list_display = ['title', 'category', 'owner', 'status',
                     'create_time', 'operator']
@@ -118,14 +119,6 @@ class PostAdmin(admin.ModelAdmin):
             reverse('cus_admin:blog_post_change', args=(obj.id, ))
         )
     operator.short_description = '操作'
-
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(PostAdmin, self).save_model(request, obj, form, change)
-
-    def get_queryset(self, request):
-        qs = super(PostAdmin, self).get_queryset(request)
-        return qs.filter(owner=request.user)
 
     # 自定义编辑页css和js样式
     class Media:
---
+++ typeidea/typeidea/base_admin.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/python3
+'''
+@File: base_admin
+@time:2023/1/29
+@Author:majiaqin 170479
+@Desc:抽象django的基类供所有app调用
+'''
+from django.contrib import admin
+
+class BaseOwnerAdmin(admin.ModelAdmin):
+    '''
+    1.用来自动补充文章,分类,标签,侧边栏,友链这些Model的owner字段
+    2.用来针对queryset过滤当前用户的数据
+    '''
+    exclude = ('owner',)
+
+    def get_queryset(self, request):
+        qs = super(BaseOwnerAdmin, self).get_queryset(request)
+        return qs.filter(owner=request.user)
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(BaseOwnerAdmin, self).save_model(request, obj, form, change)
